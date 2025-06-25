95dfad04c434bce56404e779e6e08962af6d2d43
Daniel Meszaros <easimer@gmail.com>
2025-06-25 04:29:00 +0200

    Configure helix

--- home/default.nix
+++ home/default.nix
@@ -13,6 +13,7 @@
     homeDirectory = "/home/easimer";
     packages = with pkgs; [
       nil
+      nixfmt
       helix
       unzip
       less
@@ -23,6 +24,10 @@
       tree
       gnutar
     ];
+
+    sessionVariables = {
+      EDITOR = "hx";
+    };
   };
 
   programs.home-manager.enable = true;
@@ -36,6 +41,27 @@
     enable = true;
     userName = "Daniel Meszaros";
     userEmail = "daniel.meszaros@r34dy.io";
+  };
+
+  programs.helix = {
+    enable = true;
+    settings = {
+      theme = "ferra";
+      editor = {
+        line-number = "absolute";
+        rulers = [80];
+        end-of-line-diagnostics = "warning";
+        inline-diagnostics = {
+          cursor-line = "error";
+          other-lines = "error";
+        };
+      };
+    };
+    languages.language = [{
+      name = "nix";
+      auto-format = true;
+      formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
+    }];
   };
 
   systemd.user.startServices = "sd-switch";
