1a27eff79cfb426597b767c720d54a40ee3d48a0
Daniel Meszaros <easimer@gmail.com>
2025-07-13 17:26:39 +0200

    Display the ip4 addr in hyperv vms

--- systems/hyperv/default.nix
+++ systems/hyperv/default.nix
@@ -52,6 +52,8 @@
   # and is only reachable from the host.
   networking.firewall.enable = false;
 
+  services.getty.helpLine = "IPv4 address: \\4";
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
