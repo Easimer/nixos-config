41abb372eea44096456d73cbcce285d3ff100de9
Daniel Meszaros <easimer@gmail.com>
2025-08-06 21:59:12 +0200

    Fixed ip4 addr for zen-hyperv

--- hosts/zen-hyperv/default.nix
+++ hosts/zen-hyperv/default.nix
@@ -6,5 +6,20 @@
     ../../utils/vscode-server
   ];
 
-  networking.hostName = "zen-hyperv";
+  networking = {
+    hostName = "zen-hyperv";
+    interfaces = {
+      eth0.ipv4.addresses = [
+        {
+          address = "172.28.48.44";
+          prefixLength = 20;
+        }
+      ];
+    };
+    defaultGateway = "172.28.48.1";
+    nameservers = [
+      "8.8.8.8"
+      "8.8.4.4"
+    ];
+  };
 }
