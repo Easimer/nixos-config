a7e889e71f9063e8b88cd380b76dfbd66db8c75b
Daniel Meszaros <easimer@gmail.com>
2025-09-23 21:19:27 +0200

    xrdp

--- hosts/hell-hyperv/default.nix
+++ hosts/hell-hyperv/default.nix
@@ -7,4 +7,43 @@
   ];
 
   networking.hostName = "hell-hyperv";
+
+  services.openvpn.servers = {
+    office = {
+      config = ''config /home/easimer/.config/openvpn/office.ovpn'';
+    };
+  };
+
+  users.users.easimer = {
+    isNormalUser = true;
+    description = "easimer";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+    packages = with pkgs; [
+      jetbrains.idea-community-bin
+      alacritty
+    ];
+  };
+
+  services.xserver = {
+    enable = true;
+    desktopManager = {
+      xterm.enable = false;
+    };
+
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+        dmenu
+        i3status
+      ];
+    };
+  };
+  services.xrdp = {
+    enable = true;
+    defaultWindowManager = "${pkgs.i3}/bin/i3";
+    openFirewall = true;
+  };
 }
--- hosts/zen-hyperv/default.nix
+++ hosts/zen-hyperv/default.nix
@@ -16,35 +16,36 @@
     };
   };
 
-  # users.users.easimer = {
-  #   isNormalUser = true;
-  #   description = "easimer";
-  #   extraGroups = [
-  #     "networkmanager"
-  #     "wheel"
-  #   ];
-  #   packages = with pkgs; [
-  #     jetbrains.idea-community-bin
-  #   ];
-  # };
+  users.users.easimer = {
+    isNormalUser = true;
+    description = "easimer";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+    packages = with pkgs; [
+      jetbrains.idea-community-bin
+      alacritty
+    ];
+  };
 
-  # services.xserver = {
-  #   enable = true;
-  #   desktopManager = {
-  #     xterm.enable = false;
-  #   };
+  services.xserver = {
+    enable = true;
+    desktopManager = {
+      xterm.enable = false;
+    };
 
-  #   windowManager.i3 = {
-  #     enable = true;
-  #     extraPackages = with pkgs; [
-  #       dmenu
-  #       i3status
-  #     ];
-  #   };
-  # };
-  # services.xrdp = {
-  #   enable = true;
-  #   defaultWindowManager = "${pkgs.i3}/bin/i3";
-  #   openFirewall = true;
-  # };
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+        dmenu
+        i3status
+      ];
+    };
+  };
+  services.xrdp = {
+    enable = true;
+    defaultWindowManager = "${pkgs.i3}/bin/i3";
+    openFirewall = true;
+  };
 }
