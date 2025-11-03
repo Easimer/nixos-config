4e7b18b9568891c57897a450c78d297768fd76a8
Daniel Meszaros <daniel.meszaros@r34dy.io>
2025-11-03 14:26:38 +0100

    frost: update hw config

--- hosts/frost/default.nix
+++ hosts/frost/default.nix
@@ -52,6 +52,10 @@
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
 
+  services.fwupd = {
+    enable = true;
+  };
+
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
@@ -136,6 +140,8 @@
   environment.systemPackages = with pkgs; [
     vim
     wget
+    pciutils
+    usbutils
   ];
 
   services.openssh = {
